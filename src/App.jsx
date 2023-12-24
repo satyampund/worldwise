f46d0d5a0f7d45c04dcba0ef5f5302f66b603e40
Satyam Pund <pundsatyam@gmail.com>
2023-12-24 22:40:46 +0530

    feat: react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import HomePage from './pages/HomePage';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+
 const App = () => {
   return (
-    <div>
-      <h1>Worldwise</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav';
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <PageNav />
+      Not Found
+    </div>
+  );
+};
+
+export default PageNotFound;
