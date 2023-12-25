c6dcd5350c07ebbdf885935bfb39d7292ae98758
Satyam Pund <pundsatyam@gmail.com>
2023-12-25 11:44:34 +0530

    feat: nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,15 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>LIST OF CITIES</p>} />
+          <Route path="cities" element={<p>LIST OF CITIES</p>} />
+          <Route path="countries" element={<p>LIST OF COURNTRIES</p>} />
+          <Route path="form" element={<p>FORM</p>} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
