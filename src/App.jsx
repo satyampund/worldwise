94ef50bdc61bb80b1e1985e92179fe0b7438ce22
Satyam Pund <pundsatyam@gmail.com>
2023-12-26 22:20:15 +0530

    feat: useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login';
 import CityList from './components/CityList';
 import City from './components/City';
 import CountryList from './components/CountryList';
+import Form from './components/Form';
 
 const BASE_URL = 'https://my-json-server.typicode.com/satyampund/worldwise';
 
@@ -43,7 +44,7 @@ const App = () => {
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<p>FORM</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
